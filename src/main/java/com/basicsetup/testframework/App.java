db78fac36fc304d7ab167c461d933c6dd3e12b66
piyush raghatate <raghatate.piyush@gmail.com>
2018-03-29 09:06:08 +1300

    Initial commit

---
+++ src/main/java/com/basicsetup/testframework/App.java
@@ -0,0 +1,13 @@
+package com.basicsetup.testframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
